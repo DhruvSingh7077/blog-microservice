562c419c584f3449afb728e0fd99f88a97d41be6
Dhruv Singh <dhruvsingh7077@gmail.com>
2025-12-12 21:17:40 +0530

    implement the multer and buffer functions

--- services/user/src/controllers/user.ts
+++ services/user/src/controllers/user.ts
@@ -2,6 +2,9 @@ import User from "../model/User.js";
 import jwt from "jsonwebtoken";
 import TryCatch from "../utils/TryCatch.js";
 import type { AuthenticatedRequest } from "../middleware/isAuth.js";
+import getBuffer from "../utils/dataUri.js";
+import { v2 as cloudinary } from "cloudinary";
+
 export const loginUser = TryCatch(async (req, res) => {
   const { name, email, image, instagram, facebook, linkedin, bio, password } =
     req.body;
@@ -66,3 +69,44 @@ export const updateUser = TryCatch(async (req: AuthenticatedRequest, res) => {
     token,
   });
 });
+
+export const updateProfilePic = TryCatch(
+  async (req: AuthenticatedRequest, res) => {
+    const file = req.file;
+
+    if (!file) {
+      res.status(400).json({
+        message: "No file to upload",
+      });
+      return;
+    }
+
+    const fileBuffer = getBuffer(file);
+    if (!fileBuffer || !fileBuffer.content) {
+      res.status(400).json({
+        message: "fail to create buffer",
+      });
+      return;
+    }
+    const cloud = await cloudinary.uploader.upload(fileBuffer.content, {
+      folder: "blogs",
+    });
+
+    const user = await User.findByIdAndUpdate(
+      req.user?._id,
+      {
+        image: cloud.secure_url,
+      },
+      { new: true }
+    );
+    const token = jwt.sign({ user }, process.env.JWT_SEC as string, {
+      expiresIn: "5d",
+    });
+
+    res.json({
+      message: " User Profile  Pic updated successfully",
+      user,
+      token,
+    });
+  }
+);
